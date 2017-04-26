f8bf44b1cd64a506f4b795c2a82c5ff8fbdd8e2c
Jordan <jordan.dahbi@gmail.com>
2017-04-26 12:19:51 +0200

    tests ok

---
+++ src/test/java/front/SeriesPage.java
@@ -0,0 +1,18 @@
+package front;
+
+
+import org.fluentlenium.core.FluentPage;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+public class SeriesPage extends FluentPage{
+    @Override
+    public String getUrl() {
+        return "http://localhost:8080/series/";
+    }
+
+    @Override
+    public void isAt() {
+        assertThat(window().title()).contains("Series");
+    }
+}
